ef6b46090f1ed6e13668df4762bf32edad94ff25
Steshenko Dmitriy <3301297@mail.ru>
2022-07-31 01:46:10 +0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HomePage from './pages/home/HomePage';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.navBar}>
+          <div className={styles.navBar__logo}>
+          
+          </div>
+          <ul className={styles.list}>
+            <li className={styles.item}><a href="#" className={styles.link}>mens</a></li>
+            <li className={styles.item}><a href="#" className={styles.link}>womans</a></li>
+            <li className={styles.item}><a href="#" className={styles.link}>kids</a></li>
+            <li className={styles.item}><a href="#" className={styles.link}>sale</a></li>
+            <li className={styles.item}><a href="#" className={styles.link}>contacts</a></li>
+          </ul>
+        </div>
+        <div className={styles.userBar}>
+          <ul className={styles.list}>
+            <li className={styles.item}><a href="#" className={styles.link}>account</a></li>
+            <li className={styles.item}><a href="#" className={styles.link}>search</a></li>
+            <li className={styles.item}><a href="#" className={styles.link}>cart</a></li>
+          </ul>
+          {/* <div className={styles.btn}>
+            
+          </div> */}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
